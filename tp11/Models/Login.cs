b8c9393f5f1e944b2287940208ebb4a54256340d
Your Name <salcedocristianit@gmail.com>
2023-11-26 00:07:54 -0300

    corregir

--- tp11/Models/Login.cs
+++
@@ -1,8 +0,0 @@
-namespace tp10;
-
-public class Login
-{
-    public string NombreDeUsuario { get; set; }
-    public string Contraseña { get; set; }
-}
-
--- tp11/Models/Usuario.cs
+++ tp11/Models/Usuario.cs
@@ -3,9 +3,7 @@ namespace tp10;
 public class Usuario{
     private int id;
     private string nombreDeUsuario;
-    private string Rol;
 
     public int Id { get => id; set => id = value; }
     public string NombreDeUsuario { get => nombreDeUsuario; set => nombreDeUsuario = value; }
-    public string Rol1 { get => Rol; set => Rol = value; }
 }
